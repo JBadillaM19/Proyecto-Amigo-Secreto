9dbf6097a6cfac1208273bb77cef91dbee568f72
Josue Badilla <josuebadillam.work@gmail.com>
2025-08-20 14:57:09 -0600

    Crea función array

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombresDeAmigos = [];
+//Creación del array
+
+
+function agregarAmigo() {
+    let nombreEscrito = document.getElementById("amigo").value;
+    //capturando el valor ingresado
+    if (nombreEscrito.trim() === "") {
+        alert ("Por favor, ingresa un nombre");
+    } else {
+        nombresDeAmigos.push(nombreEscrito);
+
+        document.getElementById("amigo").value = "";
+        console.log(nombresDeAmigos);
+    }
+}
